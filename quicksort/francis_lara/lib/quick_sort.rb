f4407299afcb985068006e1463bbb309665647ec
Francis <lara.francis1@gmail.com>
2018-01-05 10:32:09 -0800

    Add not in-place sorting

--- quicksort/francis_lara/lib/quick_sort.rb
+++ quicksort/francis_lara/lib/quick_sort.rb
@@ -4,12 +4,31 @@ class QuickSort
 
   # Not in-place. Uses O(n) memory.
   def self.sort1(array)
+    return array if array.length == 1
+
+    left = []
+    right = []
+    pivot = rand(array.length)
+
+    array.each do |el, idx|
+      if idx == pivot
+        next
+      elsif el < array[pivot]
+        left.push(el)
+      else
+        right.push(el)
+      end
+    end
+
+    QuickSort.sort1(left) + [array[pivot]] + QuickSort.sort1(right)
   end
 
   # In-place.
   def self.sort2!(array, start = 0, length = array.length, &prc)
+    
   end
 
   def self.partition(array, start, length, &prc)
+
   end
 end
