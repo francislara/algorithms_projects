9c0cc09dbc3f863a9f69e4e7b89a6656dbf0bae2
Francis <lara.francis1@gmail.com>
2018-01-05 12:08:55 -0800

    Add sorting in place

--- quicksort/francis_lara/lib/quick_sort.rb
+++ quicksort/francis_lara/lib/quick_sort.rb
@@ -1,10 +1,12 @@
+require 'byebug'
+
 class QuickSort
   # Quick sort has average case time complexity O(nlogn), but worst
   # case O(n**2).
 
   # Not in-place. Uses O(n) memory.
   def self.sort1(array)
-    return array if array.length == 1
+    return array if array.length <= 1
 
     left = []
     right = []
@@ -20,15 +22,34 @@ class QuickSort
       end
     end
 
-    QuickSort.sort1(left) + [array[pivot]] + QuickSort.sort1(right)
+    self.sort1(left) + [array[pivot]] + self.sort1(right)
   end
 
   # In-place.
   def self.sort2!(array, start = 0, length = array.length, &prc)
-    
+    prc ||= Proc.new { |a, b| a <=> b }
+    return array if length <= 1
+
+    pivot_idx = self.partition(array, start, length, &prc)
+
+    sort2!(array, start, pivot_idx - start, &prc)
+    sort2!(array, pivot_idx + 1, (length - pivot_idx + start - 1), &prc)
+
+    array
   end
 
   def self.partition(array, start, length, &prc)
+    prc ||= Proc.new { |a, b| a <=> b }
 
+    pivot = start
+
+    ((start + 1)...(start + length)).each do |i|
+      if prc.call(array[pivot], array[i]) >= 0
+        array.insert(pivot, array.delete_at(i))
+        pivot += 1
+      end
+    end
+
+    pivot
   end
 end
