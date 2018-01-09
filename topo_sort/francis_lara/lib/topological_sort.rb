c63011737069325b0d037471083272b3f744897c
Francis <lara.francis1@gmail.com>
2018-01-09 11:05:35 -0800

    Add kahn's topo sort

--- topo_sort/francis_lara/lib/topological_sort.rb
+++ topo_sort/francis_lara/lib/topological_sort.rb
@@ -2,6 +2,26 @@ require_relative 'graph'
 
 # Implementing topological sort using both Khan's and Tarian's algorithms
 
+# Kahn's Algorithm
 def topological_sort(vertices)
+  queue = []
+  list = []
+  vertices.each { |vertex| queue << vertex if vertex.in_edges.empty? }
 
+  until queue.empty?
+    removal = queue.shift
+    list << removal
+
+    removal.out_edges.each do |edge|
+      outer_vertex = edge.to_vertex
+
+      outer_vertex.in_edges.delete(edge)
+
+      queue << outer_vertex if outer_vertex.in_edges.empty?
+    end
+  end
+
+  return list if list.length == vertices.length
+
+  []
 end
